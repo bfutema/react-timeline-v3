bfd31ea1a928d4ab94141afdbf6324ab30ef1713
Bruno Futema <bruno.futema@outlook.com>
2021-10-22 12:06:07 -0300

    feat: :sparkles: Using Timeline component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,30 @@ import React from 'react';
 import { Login } from '@pages/Login';
 import { GlobalStyles } from '@styles/global';
 
+import { AppProvider } from './contexts';
+
+// import './style.css';
+
 const App: React.FC = () => {
   return (
-    <>
+    <AppProvider>
       <Login />
+
+      {/* <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="16" cy="16" r="16" fill="white" />
+        <circle cx="84" cy="16" r="16" fill="red" />
+        <circle cx="16" cy="84" r="16" fill="yellow" />
+        <circle cx="84" cy="84" r="16" fill="green" />
+
+        <path
+          d="M 16 16 H 84 V 84 H 16 L 16 16"
+          fill="purple"
+          onClick={e => console.log(e)}
+        />
+      </svg> */}
+
       <GlobalStyles />
-    </>
+    </AppProvider>
   );
 };
 
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import { Timeline } from '@components/organisms';
 
+// import { FrappeApp } from '../../solutions/Frappe/App';
+
 import { Container } from './styles';
 
 const Login: React.FC = () => {
   return (
     <Container className="login">
       <Timeline />
+
+      {/* <FrappeApp /> */}
     </Container>
   );
 };
